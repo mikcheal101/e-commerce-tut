08535364760377574fa0cf1ece2f31d3ea6ad4ff
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-10-15 00:52:37 +0100

    DiscountRepository - update - delete methods created

--- src/Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
+++ src/Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
@@ -8,7 +8,6 @@ namespace Discount.Api.Repositories
     
     public class DiscountRepository: IDiscountRepository
     {
-        private readonly IConfiguration _configuration;
 
         public DiscountRepository(IConfiguration config)
         {
@@ -17,10 +16,7 @@ namespace Discount.Api.Repositories
 
         public async Task<Coupon> GetDiscount(string productId)
         {
-            var connectionString = _configuration.GetValue<string>("DatabaseSettings:ConnectionString");
-            using var connection = new NpgsqlConnection(connectionString);
-
-            var coupon = await connection
+            var coupon = await this.Connection
                 .QueryFirstOrDefaultAsync<Coupon>(
                     "SELECT * FROM coupon WHERE productName=@productName", 
                     new { ProductName = productId });
@@ -35,10 +31,7 @@ namespace Discount.Api.Repositories
 
         public async Task<bool> CreateDiscount(Coupon coupon)
         {
-            var connectionString = _configuration.GetValue<string>("DatabaseSettings:ConnectionString");
-            using var connection = new NpgsqlConnection(connectionString);
-
-            var affected = await connection
+            var affected = await this.Connection
                 .ExecuteAsync(
                     "INSERT INTO coupon (productName, description, amount) VALUES (@productName, @description, @amount)",
                     new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Desription, ProductId = coupon.ProductId }
@@ -49,8 +42,35 @@ namespace Discount.Api.Repositories
         }
 
         public async Task<bool> DeleteDiscount(string productId)
-        {}
+        {
+            var deleted = await this.Connection
+                .ExecuteAsync(
+                    "DELETE FROM coupon WHERE productId=@productId",
+                    new { productId = productId }
+                );
+
+            return deleted > 0;
+        }
+
         public async Task<bool> UpdateDiscount(Coupon coupon)
-        {}
+        {
+            var updated = await this.Connection
+                .ExecuteAsync(
+                    "UPDATE coupon SET productName=@productName, description=@description, amount=@amount WHERE id=@id",
+                    new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Desription, ProductId = coupon.ProductId, Id = coupon.Id }
+                );
+
+            return updated > 0;
+        }
+
+
+        private readonly IConfiguration _configuration;
+        public NpgsqlConnection Connection { 
+            get
+            {
+                var connectionString = _configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+                return new NpgsqlConnection(connectionString);
+            } 
+        }
     }
 }
