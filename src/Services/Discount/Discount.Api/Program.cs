e050e6afb9b81ec788e9381a68776b428507c1aa
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-10-15 13:48:14 +0100

    dep inj

--- src/Services/Discount/Discount.Api/Program.cs
+++ src/Services/Discount/Discount.Api/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Discount.Api.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
 
